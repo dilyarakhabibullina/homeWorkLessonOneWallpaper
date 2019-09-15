8ddb7f8610f103c9cf6a7ea22a1d809c2714f8ab
dilyarakhabibullina <khdilyara@rambler.ru>
2019-09-15 21:00:31 +0300

    How to calculate wallpaper

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int roomWidth=4;
-        int roomHeight=3;
-        int roomLength=6;
+        int roomWidth = 4;
+        int roomHeight = 3;
+        int roomLength = 6;
         int rollWidthInSm = 53;
         int rollLengthInSm = 1000;
-        int roomPerimetr = (roomLength+roomWidth)*2;
-        int rollPieces = roomPerimetr*100/rollWidthInSm;
-        int piecesInOneRoll = rollLengthInSm/100/roomHeight;
-        int rollAmount = rollPieces/piecesInOneRoll+1;
-        System.out.println("Вам нужно купить "+ rollAmount+" рулонов обоев");
+        int roomPerimetr = (roomLength + roomWidth) * 2;
+        int rollPieces = roomPerimetr * 100 / rollWidthInSm;
+        int piecesInOneRoll = rollLengthInSm / 100 / roomHeight;
+        int rollAmount = rollPieces / piecesInOneRoll + 1;
+        System.out.println("Вам нужно купить " + rollAmount + " рулонов обоев");
     }
 }
