5ba3b2d81b5548868925104d586d1b543068b68e
dilyarakhabibullina <khdilyara@rambler.ru>
2019-09-21 14:47:56 +0300

    удалены папка .idea и файл *.iml

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        int roomWidth = 4;
-        int roomHeight = 3;
-        int roomLength = 6;
+        int roomWidthInSm = 400;
+        int roomHeightInSm = 300;
+        int roomLengthInSm = 600;
         int rollWidthInSm = 53;
         int rollLengthInSm = 1000;
-        int roomPerimetr = (roomLength + roomWidth) * 2;
-        int rollPieces = roomPerimetr * 100 / rollWidthInSm + 1;
-        int piecesInOneRoll = rollLengthInSm / 100 / roomHeight;
+        int roomPerimetr = (roomLengthInSm + roomWidthInSm) * 2;
+        int rollPieces = roomPerimetr / rollWidthInSm + 1;
+        int piecesInOneRoll = rollLengthInSm / roomHeightInSm;
         int rollAmount = rollPieces / piecesInOneRoll;
         System.out.println("Вам нужно купить " + rollAmount + " рулонов обоев");
     }
